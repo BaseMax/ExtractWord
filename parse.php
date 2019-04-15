819c95a5524801fc4aaa3e4e40974f778be3bb69
Max Base <MaxBaseCode@Gmail.Com>
2019-04-15 22:02:37 +0430

    Update parse.php

--- parse.php
+++ parse.php
@@ -10,38 +10,56 @@
 *
 **/
 if(isset($_POST['srt'])) {
-    header("Content-Type: text/plain");
-    $srt=$_POST['srt'];
-    $lines=explode("\n", $srt);
-    $output="";
-    // file_put_contents("s".rand(1000,9999).".txt",$srt);
-    foreach($lines as $line) {
-        $line=trim($line);
-        if($line=="") {
-            continue;
-        }        
-        $index=(int) $_POST['index'];
-        $words=explode(" ",$line);
-        $words=array_filter($words);
-        $words=array_values($words);
-        // print_r($words);
-        if($index < 0) {
-            $index=count($words) - (-1*($index));
-        }
-        else if($index > 0) {
-            $index--;
-        }
-        $line=$words[$index];
-        $output.=$line."\n";
-    }
-    print $output;
+	header("Content-Type: text/plain");
+	$srt=$_POST['srt'];
+	$filter=$_POST['filter'];
+	$lines=explode("\n", $srt);
+	$output="";
+	// file_put_contents("s".rand(1000,9999).".txt",$srt);
+	foreach($lines as $line) {
+		$line=trim($line);
+		if($line=="") {
+			continue;
+		}
+		$index=(int) $_POST['index'];
+		$words=explode(" ",$line);
+		$words=array_filter($words);
+		$words=array_values($words);
+		// print_r($words);
+		if($index < 0) {
+			$index=count($words) - (-1*($index));
+		}
+		else if($index > 0) {
+			$index--;
+		}
+		if(isset($words[$index])) {
+			$line=$words[$index];
+			if($filter != "none") {
+				if($filter == "up") {
+					$line=strtoupper($line);
+				}
+				else if($filter == "low") {
+					$line=strtolower($line);
+				}
+			}
+			// if($line != "") {
+			// }
+			$output.=$line."\n";
+		}
+	}
+	print $output;
 }
 else {
 ?>
 <form action="" method="POST">
-    <textarea name="srt"></textarea>
-    <input value="1" name="index" type="number"><br>
-    <button>Check</button>
+	<textarea cols="13" rows="20" name="srt"></textarea><br>
+	<input value="1" name="index" type="number"><br>
+	<select name="filter">
+		<option value="none">None</option>
+		<option value="low">Low</option>
+		<option value="up">Up</option>
+	</select>
+	<button>Check</button>
 </form>
 <?php
 }
