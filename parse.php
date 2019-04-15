9365dbd642c43dd3ca1d291f6ccbd3d41edde954
Max Base <MaxBaseCode@Gmail.Com>
2019-04-15 21:19:19 +0430

    Update parse.php

--- parse.php
+++ parse.php
@@ -9,3 +9,33 @@
 * @Repository : https://github.com/BaseMax/ExtractWord
 *
 **/
+if(isset($_POST['srt'])) {
+    header("Content-Type: text/plain");
+    $srt=$_POST['srt'];
+    $lines=explode("\n", $srt);
+    $output="";
+    // file_put_contents("s".rand(1000,9999).".txt",$srt);
+    foreach($lines as $line) {
+        $line=trim($line);
+        if($line=="") {
+            continue;
+        }
+        $index=(int) $_POST['index'];
+        $words=explode(" ",$line);
+        if($index < 0) {
+            $index=count($words) - (-1*$index);
+        }
+        $line=$words[$index];
+        $output.=$line."\n";
+    }
+    print $output;
+}
+else {
+?>
+<form action="" method="POST">
+    <textarea name="srt"></textarea>
+    <input value="1" name="index" type="number"><br>
+    <button>Check</button>
+</form>
+<?php
+}
