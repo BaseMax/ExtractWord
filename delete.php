5ad03b44bc66b3dfa665b8bc0bfc08d6ff7e83a6
Max Base <MaxBaseCode@Gmail.Com>
2019-04-15 22:03:39 +0430

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,11 @@
-
+<?php
+/**
+*
+* @Name : ExtractWord/delete.php
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-04-15
+* @Released under : https://github.com/BaseMax/ExtractWord/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/ExtractWord
+*
+**/
